598e9ac6cf4ddbf15bccf2a1b772204fcaefd999
Brandon <brandonnjenga294@gmail.com>
2025-12-15 22:02:39 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,19 +3,24 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
-// 🔥 YOUR FIREBASE CONFIG (PASTE YOURS HERE)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
+  apiKey: "AIzaSyCTAbsRYKgzmwB3Z90fY_vnph-5I8p4c_c",
+  authDomain: "g-a-limited.firebaseapp.com",
   projectId: "g-a-limited",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "g-a-limited.appspot.com",
+  messagingSenderId: "214114713672",
+  appId: "1:214114713672:web:054c2520de37fa80b2187d",
+  measurementId: "G-RQP52WK4H9"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exports for use in other scripts
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
